c168d5409b3a1c417789eab113b85da35b781dca
奈美子 髙野 <nstakano09@gmail.com>
2015-09-07 13:20:10 +0900

    プロジェクト新規作成

---
+++ SampleWinFormTest/CalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleWinForm;
+
+namespace SampleWinFormTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private Calculator calculator = new Calculator();
+
+        [TestMethod]
+        public void 和_1足す2は3であるべき()
+        {
+            Assert.AreEqual(3, this.calculator.Add(1, 2));
+        }
+
+        [TestMethod]
+        public void 差_5引く2は3であるべき()
+        {
+            Assert.AreEqual(3, this.calculator.Subtract(5, 2));
+        }
+
+        [TestMethod]
+        public void 積_3掛ける3は9であるべき()
+        {
+            Assert.AreEqual(9, this.calculator.Multiply(3, 3));
+        }
+
+        [TestMethod]
+        public void 商_6割る2は3であるべき()
+        {
+            Assert.AreEqual(3, this.calculator.Divide(6, 2));
+        }
+    }
+}
